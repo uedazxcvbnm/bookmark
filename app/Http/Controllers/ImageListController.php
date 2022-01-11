b5d1292e20739ee0c551f9f94ec4d047880462b2
Naoki00712 <kd1299709@st.kobedenshi.ac.jp>
2022-01-11 11:32:57 +0900

    v1.3

--- app/Http/Controllers/ImageListController.php
+++ app/Http/Controllers/ImageListController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class ImageListController extends Controller
 {
     //
+    function show(){
+		//アップロードした画像を取得
+		$uploads = UploadImage::orderBy("id", "desc")->get();
+
+		return view("image_list",[
+			"images" => $uploads
+		]);
+	}
 }
