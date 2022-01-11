1dc43c1c028cdb55b3ebadfdd3d2ef63fb98876e
Naoki00712 <kd1299709@st.kobedenshi.ac.jp>
2022-01-11 11:35:19 +0900

    v1.4

---
+++ app/Models/UploadImage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UploadImage extends Model
+{
+    use HasFactory;
+
+    protected $table = "upload_image";
+    protected $fillable = ["file_name","file_path"];
+}
