009896a5bd061dbb55e51f45310f0c296530e3db
nagata127 <kd1305813@st.kobedenshi.ac.jp>
2022-01-31 10:47:37 +0900

    models tag追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,6 +19,11 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     // public function uploadimages()
     // {
     //     return $this->hasMany(UploadImage::class);
